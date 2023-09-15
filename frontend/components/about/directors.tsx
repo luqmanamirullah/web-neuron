a59460c187882586a88146b726954b99172bea15
refi <refihikman06@gmail.com>
2023-09-15 09:24:41 +0700

    feat: directors fix

--- frontend/components/about/directors.tsx
+++ frontend/components/about/directors.tsx
@@ -2,48 +2,18 @@
 
 import React, { useState } from 'react';
 import Image from 'next/image';
-
-type DirectorData = {
-  id: number;
-  name: string;
-  role: string;
-};
-
-const data: DirectorData[] = [
-  {
-    id: 1,
-    name: 'Sriyanto',
-    role: 'Director',
-  },
-  {
-    id: 2,
-    name: 'Sriyanto',
-    role: 'Director',
-  },
-  {
-    id: 3,
-    name: 'Sriyanto',
-    role: 'Director',
-  },
-  {
-    id: 4,
-    name: 'Sriyanto',
-    role: 'Director',
-  },
-  {
-    id: 5,
-    name: 'Sriyanto',
-    role: 'Director',
-  },
-];
+import Link from 'next/link';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import SvgIcon from '@mui/material/SvgIcon';
+import { directorsData } from '@/data/directors';
 
 const Directors = () => {
   const [isExpand, setExpand] = useState<number>(1);
 
   return (
-    <div className="w-full flex gap-6">
+    <div className="w-full flex md:flex-row xs:flex-col md:gap-6 xs:gap-2">
       {/* Director Card */}
-      {data.map((item, index) => {
+      {directorsData.map((item, index) => {
         return (
           <div
             key={index}
@@ -53,34 +23,37 @@ const Directors = () => {
             <Image
               className={
                 isExpand === item.id
-                  ? `transition-all h-[400px] w-[436px] object-cover`
-                  : `transition-all h-[400px] w-[200px] object-cover`
+                  ? `transition-all md:h-[25rem] xs:h-[19.75rem] md:w-[27.25rem] xs:w-full object-cover`
+                  : `transition-all md:h-[25rem] xs:h-[19.75rem] md:w-[12.5rem] xs:w-full object-cover`
               }
               width={500}
               height={500}
-              src="/assets/images/director.jpg"
+              src={item.image}
               alt="Neuron Director"
             />
 
             {/* Card Content Container */}
-            <div className="absolute bottom-0 text-white w-full h-[60%] p-6 flex flex-col gap-1 justify-end bg-gradient-to-t from-red-800 via-transparent to-transparent">
+            <div className="absolute bottom-0 text-white w-full h-[70%] p-6 flex flex-col gap-1 justify-end bg-gradient-to-t from-red-800 via-transparent to-transparent">
               <div
                 className={
                   isExpand === item.id
-                    ? `transition-all`
-                    : `transition-all translate-y-28`
+                    ? `transition-all flex flex-col md:gap-1 xs:gap-2`
+                    : `transition-all md:translate-y-28 xs:translate-y-0 flex flex-col md:gap-1 xs:gap-2`
                 }
               >
-                <h5 className="text-2xl font-bold">{item.name}</h5>
+                <h5 className="text-desktop-title font-raleway font-bold">
+                  {item.name}
+                </h5>
+
                 <div className="flex justify-between">
-                  <span className="text-base font-normal">{item.role}</span>
-                  <Image
-                    className="h-[24px] object-cover text-white"
-                    width={24}
-                    height={24}
-                    src="/assets/images/linkedin.png"
-                    alt="Linkedin Logo"
-                  />
+                  <span className="text-desktop-body-small font-inter font-normal">
+                    {item.role}
+                  </span>
+                  <Link href="#">
+                    <SvgIcon>
+                      <LinkedInIcon />
+                    </SvgIcon>
+                  </Link>
                 </div>
               </div>
             </div>
---
+++ frontend/data/directors.ts
@@ -0,0 +1,39 @@
+interface Directors {
+  id: number;
+  name: string;
+  role: string;
+  image: string;
+}
+
+export const directorsData: Directors[] = [
+  {
+    id: 1,
+    name: 'Sriyanto',
+    role: 'Director',
+    image: '/assets/images/director.jpg',
+  },
+  {
+    id: 2,
+    name: 'Sriyanto',
+    role: 'Director',
+    image: '/assets/images/director.jpg',
+  },
+  {
+    id: 3,
+    name: 'Sriyanto',
+    role: 'Director',
+    image: '/assets/images/director.jpg',
+  },
+  {
+    id: 4,
+    name: 'Sriyanto',
+    role: 'Director',
+    image: '/assets/images/director.jpg',
+  },
+  {
+    id: 5,
+    name: 'Sriyanto',
+    role: 'Director',
+    image: '/assets/images/director.jpg',
+  },
+];
