e7af20c0b63e807d3a0a104e9b48c9b66f10b90c
refi <refihikman06@gmail.com>
2023-09-15 09:43:14 +0700

    feat(on progress): home hero CTA

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,19 +1,39 @@
 import Button from '@/components/button';
-import ArrowOutwardRounded from '@mui/icons-material/ArrowOutwardRounded';
+import ArrowForwardRounded from '@mui/icons-material/ArrowForwardRounded';
+
 function page(): JSX.Element {
   return (
-    <main className="w-full">
-      {/* Hero Section */}
-      <section className="w-full border-2 border-black h-screen flex flex-col justify-center items-center">
-        <Button
-          label="We Are Worth"
-          size="sm"
-          buttonStyle="text"
-          withIcon
-          icon={<ArrowOutwardRounded />}
-        />
+    <main className="overflow-x-hidden bg-sys-light-surface">
+      {/* NAVBAR */}
+      <div className="absolute w-full md:h-48 xs:h-[4.125rem] border-b-4"></div>
 
-        <h1 className="text-button-large"> Hello World</h1>
+      {/* Section: HERO */}
+      <section className="h-screen w-full flex items-center md:mx-xl xs:mx-xs">
+        <div className="max-w-[48.625rem] mt-[2.12rem]">
+          {/* Display CTA */}
+          <div className="flex flex-col text-desktop-display font-bold">
+            <h1>Enhance Your Business:</h1>
+            <div className="flex gap-6">
+              <h1 className="bg-">Simplify</h1>
+              <h1>With Neuronworks</h1>
+            </div>
+            <h1>Software Evolution</h1>
+          </div>
+
+          {/* Desc */}
+          <p className="text-desktop-body-large mt-2 mb-10">
+            Where Vision Transforms into Code Empowering Your Digital Future
+          </p>
+
+          {/* Btn */}
+          <Button
+            label="CONSULT WITH US"
+            size="lg"
+            buttonStyle="filled"
+            withIcon={true}
+            icon={<ArrowForwardRounded />}
+          />
+        </div>
       </section>
     </main>
   );
