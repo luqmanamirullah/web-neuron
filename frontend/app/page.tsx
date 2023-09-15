c7a7600b5ba664a0e4ca137dcffb53296f160b66
luqmanamirullah <amirullahluqman@gmail.com>
2023-09-15 09:30:30 +0700

    refactor: implement navbar

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,21 +1,9 @@
-import Button from '@/components/button';
-import ArrowOutwardRounded from '@mui/icons-material/ArrowOutwardRounded';
 function page(): JSX.Element {
   return (
-    <main className="w-full">
+    <>
       {/* Hero Section */}
-      <section className="w-full border-2 border-black h-screen flex flex-col justify-center items-center">
-        <Button
-          label="We Are Worth"
-          size="sm"
-          buttonStyle="text"
-          withIcon
-          icon={<ArrowOutwardRounded />}
-        />
-
-        <h1 className="text-button-large"> Hello World</h1>
-      </section>
-    </main>
+      <section className="w-full h-[200vh] flex flex-col justify-center items-center"></section>
+    </>
   );
 }
 
