fbaa8684a352505aa38d35c3dfd533d248949149
refi <refihikman06@gmail.com>
2023-09-15 09:30:11 +0700

    feat: values data map

--- frontend/components/about/values.tsx
+++ frontend/components/about/values.tsx
@@ -1,75 +1,28 @@
 import React from 'react';
 import Heading from '../heading';
+import { valueData } from '@/data/values';
 
-type Props = {};
-
-const Values = (props: Props) => {
+const Values = () => {
   return (
-    <section className="h-screen mx-10 mt-[120px] flex flex-col gap-16">
+    <section className="h-fit md:mx-xl xs:mx-xs mt-[7.5rem] flex flex-col md:gap-16 xs:gap-8">
       <Heading
         darkBg={false}
         heading={'Foundation of Our Core Values'}
         subheading={'Our Values'}
       />
-      <ul className="w-full grid grid-cols-3 gap-x-6 gap-y-10">
-        <li>
-          <h5 className="text-[25px] font-bold leading-[32px] text-center mb-2">
-            INNOVATION FOR SOLUTIONS
-          </h5>
-          <p className="text-[18px] font-normal leading-[26px] text-center">
-            Every challenge is an opportunity, and every solution is a step
-            toward progress.
-          </p>
-        </li>
-
-        <li>
-          <h5 className="text-[25px] font-bold leading-[32px] text-center mb-2">
-            CUSTOMER SATISFACTION
-          </h5>
-          <p className="text-[18px] font-normal leading-[26px] text-center">
-            Delivering Professional Service with a Focus on Win-Win Solutions,
-            Ensuring Rapid Responses within 24 Hours.
-          </p>
-        </li>
-
-        <li>
-          <h5 className="text-[25px] font-bold leading-[32px] text-center mb-2">
-            RELIABLE APPLICATION
-          </h5>
-          <p className="text-[18px] font-normal leading-[26px] text-center">
-            We are committed to always focusing and being disciplined in
-            adhering to scientific principles in developing reliable software to
-            create zero-bug software.
-          </p>
-        </li>
-
-        <li>
-          <h5 className="text-[25px] font-bold leading-[32px] text-center mb-2">
-            EFFECTIVE TEAMWORK
-          </h5>
-          <p className="text-[18px] font-normal leading-[26px] text-center">
-            An environment of collaborative excellence, mutual respect, and
-            common objectives
-          </p>
-        </li>
-
-        <li>
-          <h5 className="text-[25px] font-bold leading-[32px] text-center mb-2">
-            INNOVATE IN TECHNOLOGICAL ADVANCES
-          </h5>
-          <p className="text-[18px] font-normal leading-[26px] text-center">
-            Explore emerging technologies and ideate creative solutions.
-          </p>
-        </li>
-
-        <li>
-          <h5 className="text-[25px] font-bold leading-[32px] text-center mb-2">
-            RESPONSIBILITY AND PRIORITY IN EVERY PROJECT
-          </h5>
-          <p className="text-[18px] font-normal leading-[26px] text-center">
-            Accountable for every aspect of a project
-          </p>
-        </li>
+      <ul className="w-full grid md:grid-cols-3 xs:grid-cols-1 md:gap-x-6 xs:gap-x-0 gap-y-10">
+        {valueData.map((item) => {
+          return (
+            <li>
+              <h5 className="md:text-desktop-title xs:text-mobile-title font-bold text-center md:mb-2 xs:mb-1">
+                {item.title}
+              </h5>
+              <p className="md:text-desktop-body xs:text-mobile-body font-normal text-center">
+                {item.desc}
+              </p>
+            </li>
+          );
+        })}
       </ul>
     </section>
   );
