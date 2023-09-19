eaa50132083cb845b48d25324c019b9c582a0f32
sannhaq <ikhsannhaq@gmail.com>
2023-09-19 09:18:59 +0700

    feat: Migrations Career Feature

--- backend/database/migrations/2023_09_18_022602_make_name_nullable_in_job_plus_value_table.php
+++ backend/database/migrations/2023_09_18_022602_make_name_nullable_in_job_plus_value_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class AddOurSolutionAndDetailsToPortofoliosTable extends Migration
+class MakeNameNullableInJobPlusValueTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,9 @@ class AddOurSolutionAndDetailsToPortofoliosTable extends Migration
      */
     public function up()
     {
-        // Schema::table('portofolios', function (Blueprint $table) {
-        //     $table->text('our_solution');
-        //     $table->text('details');
-        // });
+        Schema::table('job_plus_value', function (Blueprint $table) {
+            $table->string('name')->nullable()->change();
+        });
     }
 
     /**
@@ -26,7 +25,7 @@ class AddOurSolutionAndDetailsToPortofoliosTable extends Migration
      */
     public function down()
     {
-        Schema::table('portofolios', function (Blueprint $table) {
+        Schema::table('job_plus_value', function (Blueprint $table) {
             //
         });
     }
