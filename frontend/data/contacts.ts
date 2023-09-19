91db0aa4998e719f7ed508f7c02bbd59599c7723
luqmanamirullah <amirullahluqman@gmail.com>
2023-09-19 08:07:58 +0700

    feat(dummy): contact data & type

---
+++ frontend/data/contacts.ts
@@ -0,0 +1,25 @@
+interface Contact {
+  id: number;
+  name: string;
+  value: string;
+}
+
+const contacts: Contact[] = [
+  {
+    id: 1,
+    name: 'Phone',
+    value: '(022)-87309898',
+  },
+  {
+    id: 2,
+    name: 'Email',
+    value: 'support@neuronworks.co.id',
+  },
+  {
+    id: 3,
+    name: 'WhatsApp',
+    value: '+62 812 2333 2222',
+  },
+];
+
+export default contacts;
