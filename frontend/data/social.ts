b9a9138cca2e0bc25655805602064baf7a38f5c8
luqmanamirullah <amirullahluqman@gmail.com>
2023-09-19 08:09:05 +0700

    feat: custom new twitter icon

---
+++ frontend/data/social.ts
@@ -0,0 +1,16 @@
+interface Social {
+  facebook: string;
+  twitter: string;
+  instagram: string;
+  linkedin: string;
+  youtube: string;
+}
+
+const social: Social = {
+  facebook: 'https://www.facebook.com/your-facebook-page',
+  twitter: 'https://www.twitter.com/your-twitter-page',
+  instagram: 'https://www.instagram.com/your-instagram-page',
+  linkedin: 'https://www.linkedin.com/your-linkedin-page',
+  youtube: 'https://www.youtube.com/your-youtube-page',
+};
+export default social;
