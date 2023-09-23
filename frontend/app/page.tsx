869dab61c27cef68f261a1a4d882489bcfb0097a
luqmanamirullah <amirullahluqman@gmail.com>
2023-09-24 01:05:02 +0700

    feat: try to use client

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,11 @@
+'use client';
 function page(): JSX.Element {
   return (
     <>
       {/* Hero Section */}
-      <section className="w-full h-[200vh] flex flex-col justify-center items-center py-40"></section>
+      <section className="w-full h-[200vh] flex flex-col justify-center items-center py-40">
+        <h1>Hello</h1>
+      </section>
     </>
   );
 }
