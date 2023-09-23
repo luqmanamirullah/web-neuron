cb35b1def252513432f1b197279a0fdeed9261fb
luqmanamirullah <amirullahluqman@gmail.com>
2023-09-24 00:03:37 +0700

    fix: base layout

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,7 +2,7 @@ function page(): JSX.Element {
   return (
     <>
       {/* Hero Section */}
-      <section className="w-full h-[200vh] flex flex-col justify-center items-center"></section>
+      <section className="w-full h-[200vh] flex flex-col justify-center items-center py-40"></section>
     </>
   );
 }
