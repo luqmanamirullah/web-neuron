e3e495f0c21af83120742eefc51784b67c1a49f2
sannhaq <ikhsannhaq@gmail.com>
2023-09-24 18:33:14 +0700

    fix: Portofolio API $ CMS

--- backend/app/Http/Controllers/PortofolioController.php
+++ backend/app/Http/Controllers/PortofolioController.php
@@ -89,7 +89,7 @@ class PortofolioController extends Controller
             'details' => 'required|string',
             'created_at' => 'required|date',
             'successProject' => Rule::in(['true', 'false']),
-            'technologies' => 'required|array',
+            'technologies' => 'nullable|array',
             'deliverables' => 'required|array',
             'handles' => 'required|array',
         ]);
