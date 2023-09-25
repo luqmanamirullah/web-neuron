8cf76c0bcdcc91da17631a2fe130e37a1c914af6
refi <refihikman06@gmail.com>
2023-09-25 17:29:48 +0700

    feat: using react-spring for strategic plan accordion

--- frontend/components/about/strategicPlan.tsx
+++ frontend/components/about/strategicPlan.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import React, { useState } from 'react';
+import { useSpring, config } from '@react-spring/web';
 import StrategicPlanList from './strategicPlanList';
 import ArrowDropDownRounded from '@mui/icons-material/ArrowDropDownRounded';
 import SvgIcon from '@mui/material/SvgIcon';
@@ -12,16 +13,28 @@ interface Props {
 }
 
 const StrategicPlan = ({ aboutData }: Props) => {
-  const [isActive, setIsActive] = useState<number>(0);
+  const [isActive, setIsActive] = useState<number>(-1);
+
+  const toggleActive = (id: number) => {
+    setIsActive(isActive === id ? -1 : id);
+  };
 
   return (
     <div className="flex flex-col gap-4">
-      {aboutData.data.management_strategies.map((item: any) => {
+      {aboutData.data.management_strategies.map((item: any, index: number) => {
+        const isItemActive = isActive === index;
+
+        const contentSpring = useSpring({
+          maxHeight: isItemActive ? '1000px' : '0px',
+          opacity: isItemActive ? 1 : 0,
+          config: { ...config.gentle, duration: 200 },
+        });
+
         return (
-          <div className="w-full border-[0.5px] border-sys-dark-onSurface rounded md:px-6 xs:px-4 md:py-4 xs:py-2 flex flex-col transition-all duration-[400ms]">
+          <div className="w-full border-[0.5px] border-sys-dark-onSurface rounded md:px-6 xs:px-4 md:py-4 xs:py-2 flex flex-col transition-all duration-[200ms]">
             {/* Plan Title */}
             <div
-              onClick={() => setIsActive(item.id)}
+              onClick={() => toggleActive(index)}
               className="flex items-center justify-between cursor-pointer"
             >
               <h1 className="md:text-desktop-headline xs:text-mobile-headline font-bold text-white">
@@ -31,7 +44,7 @@ const StrategicPlan = ({ aboutData }: Props) => {
               <SvgIcon
                 fontSize="large"
                 className={
-                  isActive === item.id
+                  isActive === index
                     ? 'text-white transition duration-[600ms] -rotate-180'
                     : 'text-white transition duration-[600ms]'
                 }
@@ -40,7 +53,12 @@ const StrategicPlan = ({ aboutData }: Props) => {
               </SvgIcon>
             </div>
 
-            <StrategicPlanList strategyData={item} active={isActive} />
+            <StrategicPlanList
+              strategyIndex={index}
+              springConfig={contentSpring}
+              strategyData={item}
+              active={isActive}
+            />
           </div>
         );
       })}
--- frontend/components/about/strategicPlanList.tsx
+++ frontend/components/about/strategicPlanList.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
+import { animated } from '@react-spring/web';
 
 interface Props {
+  springConfig: any;
   strategyData: any;
   active: number;
+  strategyIndex: number;
 }
 
-const StrategicPlanList = ({ strategyData, active }: Props) => {
+const StrategicPlanList = ({
+  springConfig,
+  strategyData,
+  strategyIndex,
+  active,
+}: Props) => {
   return (
-    <div
-      className={`overflow-hidden grid md:grid-cols-4 xs:grid-cols-1 md:gap-8 xs:gap-4 text-sys-dark-onSurface duration-[600ms] transition-all ${
-        active === strategyData.id
-          ? 'mt-6 md:mb-4 xs:mb-2'
-          : 'h-0 opacity-0 -translate-y-4'
+    <animated.div
+      style={springConfig}
+      className={`overflow-hidden grid md:grid-cols-4 xs:grid-cols-1 md:gap-8 xs:gap-4 text-sys-dark-onSurface duration-[200ms] transition-all ${
+        active === strategyIndex ? 'mt-6 md:mb-4 xs:mb-2' : ''
       }`}
     >
       {strategyData.management_strategy_lists.map(
@@ -28,7 +35,7 @@ const StrategicPlanList = ({ strategyData, active }: Props) => {
           );
         },
       )}
-    </div>
+    </animated.div>
   );
 };
 
