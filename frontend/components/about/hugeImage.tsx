7650a0340728bf4993d08062dd69f7f6ad672d04
refi <refihikman06@gmail.com>
2023-09-25 16:36:19 +0700

    refactor: svg using tsx

--- frontend/components/about/hugeImage.tsx
+++ frontend/components/about/hugeImage.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image';
 import React from 'react';
 import TechLines from '../svg/techLines';
 import TechCirclesAndSquares from '../svg/techCirclesAndSquares';
+import AboutHugeImage1 from '../svg/aboutHugeImage1';
+import AboutHugeImage2 from '../svg/aboutHugeImage2';
 
 interface Props {
   imageUrl: string;
@@ -18,7 +20,7 @@ const HugeImage: React.FC<Props> = ({ imageUrl }) => {
 
       <div className="relative overflow-hidden md:rounded-lg xs:rounded">
         <Image
-          className="md:max-h-[38.375rem] w-[72.75rem] object-cover"
+          className="md:max-h-[38.375rem] w-[72.75rem]"
           width={1000}
           height={1000}
           src={imageUrl}
@@ -26,26 +28,14 @@ const HugeImage: React.FC<Props> = ({ imageUrl }) => {
         />
 
         <div className="absolute md:w-72 xs:w-40 md:bottom-10 xs:bottom-4 md:left-8 xs:left-4">
-          <Image
-            className="object-cover"
-            width={300}
-            height={300}
-            src="/assets/images/Group.svg"
-            alt="Neuron Activity"
-          />
+          <AboutHugeImage1 className="" />
 
           <h6 className="absolute md:text-desktop-title xs:text-mobile-title text-white md:left-8 xs:left-4 top-[50%] translate-y-[-50%]">
             Neuron 2023
           </h6>
         </div>
 
-        <Image
-          className="absolute md:top-6 xs:top-3 max-h-[38.375rem] w-full object-cover md:px-6 xs:px-4"
-          width={500}
-          height={500}
-          src="/assets/images/Group1.svg"
-          alt="Neuron Activity"
-        />
+        <AboutHugeImage2 className="absolute md:top-6 xs:top-4 md:px-6 xs:px-4" />
       </div>
     </section>
   );
